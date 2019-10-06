1a1fd26d696b701557814a505f6bdf2060fb6d64
Paha503 <50689195+Paha503@users.noreply.github.com>
2019-10-06 22:03:41 +0600

    Хрень

--- КультинC++Proj/Площадь прямоугольника/Площадь прямоугольника/Form1.h
+++ КультинC++Proj/Площадь прямоугольника/Площадь прямоугольника/Form1.h
@@ -74,7 +74,7 @@ namespace Площадьпрямоугольника {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(251, 197);
+			this->button1->Location = System::Drawing::Point(358, 176);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(139, 41);
 			this->button1->TabIndex = 0;
@@ -85,11 +85,11 @@ namespace Площадьпрямоугольника {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Mistral", 27.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 27.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(204)));
 			this->label1->Location = System::Drawing::Point(12, 24);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(635, 44);
+			this->label1->Size = System::Drawing::Size(867, 42);
 			this->label1->TabIndex = 1;
 			this->label1->Text = L"ВЫЧИСЛИТЬ ПЛОЩАДЬ ПРЯМОУГОЛЬНИКА";
 			// 
@@ -103,14 +103,14 @@ namespace Площадьпрямоугольника {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(248, 106);
+			this->textBox1->Location = System::Drawing::Point(358, 98);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(141, 20);
 			this->textBox1->TabIndex = 3;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(248, 139);
+			this->textBox2->Location = System::Drawing::Point(358, 131);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(141, 20);
 			this->textBox2->TabIndex = 4;
@@ -118,7 +118,7 @@ namespace Площадьпрямоугольника {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(224, 109);
+			this->label3->Location = System::Drawing::Point(334, 101);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(18, 13);
 			this->label3->TabIndex = 5;
@@ -127,7 +127,7 @@ namespace Площадьпрямоугольника {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(225, 139);
+			this->label4->Location = System::Drawing::Point(335, 131);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(18, 13);
 			this->label4->TabIndex = 6;
@@ -137,7 +137,7 @@ namespace Площадьпрямоугольника {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(641, 324);
+			this->ClientSize = System::Drawing::Size(879, 324);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->textBox2);
@@ -147,6 +147,7 @@ namespace Площадьпрямоугольника {
 			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -154,13 +155,16 @@ namespace Площадьпрямоугольника {
 #pragma endregion
 	public: System::Void Variables()
 	{
-		float x =(float)(Convert::ToDouble(textBox1->Text));
-		float y =(float)(Convert::ToDouble(textBox2->Text));
-		float s = x*y;
-		string text = Convert::ToString(s);
+		int x =int::Parse(textBox1->Text);
+		int y =int::Parse(textBox2->Text);
+		int s = x * y;
 	}
-	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {label2->Text = L"Площадь равна: {0} см.", s;
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+				 label2->Text = "Площадь равна: "+s::ToString()+" см.";
 			 }
+private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
+		
+		 }
 };
 }
 
