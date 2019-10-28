a00f3fc1e72c19b746fb5ea5386b5fa68995236f
Paha503 <50689195+Paha503@users.noreply.github.com>
2019-10-28 15:27:41 +0600

    аыаыафаыф

--- C#/КультинC#Proj/Просмотр Иллюстраций 2/Просмотр Иллюстраций 2/Form1.Designer.cs
+++ C#/КультинC#Proj/Просмотр Иллюстраций 2/Просмотр Иллюстраций 2/Form1.Designer.cs
@@ -28,10 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,26 +46,31 @@
             this.button1.Size = new System.Drawing.Size(66, 21);
             this.button1.TabIndex = 0;
             this.button1.Text = "Обзор";
+            this.toolTip1.SetToolTip(this.button1, "Выбор папки");
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(151, 289);
+            this.button2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(173, 286);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(60, 26);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "◀";
+            this.toolTip2.SetToolTip(this.button2, "Назад");
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(217, 289);
+            this.button3.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(239, 286);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(60, 27);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "▶";
+            this.toolTip3.SetToolTip(this.button3, "Далее");
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -95,6 +104,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
     }
 }
 
