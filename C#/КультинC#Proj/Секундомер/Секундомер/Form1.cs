6f92bbe7805fed8929f5364cdad795f624f30e24
Paha503 <50689195+Paha503@users.noreply.github.com>
2019-10-28 14:44:59 +0600

    1

--- C#/КультинC#Proj/Секундомер/Секундомер/Form1.cs
+++ C#/КультинC#Proj/Секундомер/Секундомер/Form1.cs
@@ -11,18 +11,18 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        int m, s, ms;
+        int m, s, ms;//собсна минуты секунды и миллисекунды(а не часы)
         public Form1()
         {
             InitializeComponent();
-            timer1.Interval = 500;
-            m = 0; s = 0; ms = 0;
+            timer1.Interval = 500;//сигнал каждые полсекунды
+            m = 0; s = 0; ms = 0;//ОБНУЛЕНИЕ
             label1.Text = "00";
             label2.Text = "00";
             label3.Visible = true;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)//кнопка пуск и стоп
         {
             if (timer1.Enabled)
             {
@@ -38,16 +38,16 @@ namespace WindowsFormsApplication1
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)//кнопка сброс
         {
-            m = 0; s = 0; ms = 0;
+            m = 0; s = 0; ms = 0;//ОБНУЛЕНИЕ
             label1.Text = "00";
             label2.Text = "00";
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)//сигнал таймера
         {
-            if (label3.Visible)
+            if (label3.Visible)//двоеточие мигает с интервалом в полсекунды
             {
                 if (s < 59)
                 {
