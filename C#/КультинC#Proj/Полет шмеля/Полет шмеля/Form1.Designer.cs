b34a074174c04e4e8389703bfadbac7f800cc797
Paha503 <50689195+Paha503@users.noreply.github.com>
2019-11-01 15:48:01 +0600

    55

---
+++ C#/КультинC#Proj/Полет шмеля/Полет шмеля/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ C#/КультинC#Proj/Полет шмеля/Полет шмеля/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        System.Drawing.Bitmap flower, bee;//шмель и цветы
+        Graphics g;//холст
+
+        int dx;//скорость шмеля
+        Rectangle rct;//нахождение шмеля
+        Boolean demo = true;
+        System.Random rnd;//генератор случайных чисел
+        public Form1()
+        {
+            InitializeComponent();
+
+            try//прогрузка изображений
+            {
+                flower = new Bitmap("flower.bmp");
+                bee = new Bitmap("bee.bmp");
+                this.BackgroundImage = new Bitmap("flower.bmp");
+            }
+            catch (Exception exception)//изображений нет
+            {
+                MessageBox.Show(exception.ToString(),
+                    "Полет шмеля", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+            bee.MakeTransparent();
+            this.ClientSize = new System.Drawing.Size(new Point(BackgroundImage.Width, BackgroundImage.Height));
+            g = Graphics.FromImage(BackgroundImage);
+            rnd = new System.Random();
+            rct.X = -40;//исходные координаты шмеля
+            rct.Y = 20 + rnd.Next(20);
+            rct.Width = bee.Width;
+            rct.Height = bee.Height;
+            dx = 2;//скорость полета 2 пикселя
+            timer2.Interval = 20;
+            timer2.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            g.DrawImage(flower, new Point(0, 0));
+            if (rct.X < this.ClientRectangle.Width)
+                rct.X += dx;
+            else //если дошли до границы все заново
+            {
+                rct.X = -40;
+                rct.Y = 20 + rnd.Next(40);
+                dx = 2 + rnd.Next(4);
+            }
+            g.DrawImage(bee, rct.X, rct.Y);
+            if (!demo)
+                this.Invalidate(rct);
+            else 
+            {
+                Rectangle reg = new Rectangle(20, 20, flower.Width - 40, flower.Height - 40);
+                g.DrawRectangle(Pens.Black, reg.X, reg.Y, reg.Width - 1, reg.Height - 1);
+                this.Invalidate(reg);
+            }
+        }
+    }
+}
